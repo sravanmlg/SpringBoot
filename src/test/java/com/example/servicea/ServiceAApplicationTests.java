055bb5bf92c7de12579dfbcff47cd688aaf78d6e
himanvi vanamala <himanvivanamala@himanvis-MBP.lan>
2025-10-16 23:19:48 +0530

    Add all project files

---
+++ src/test/java/com/example/servicea/ServiceAApplicationTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.example.servicea.ClientOnboardingApplication;
+
+@SpringBootTest(classes = ClientOnboardingApplication.class)
+class ServiceAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
